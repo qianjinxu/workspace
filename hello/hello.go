4df21155e5068b04eb191e7ffe8314cd1739e111
Qianjin Xu <xuqianjinchn@gmail.com>
2023-12-27 18:48:47 +0800

    Create Go Workspace

---
+++ hello/hello.go
@@ -0,0 +1,16 @@
+/*
+ * GitHub: github.com/qianjinxu
+ * Email: xuqianjinchn@gmail.com
+ * Bio: https://jin.bio
+ */
+
+package main
+
+import (
+	"fmt"
+	"golang.org/x/example/hello/reverse"
+)
+
+func main() {
+	fmt.Println(reverse.String("Hello, Qianjin Xu!"), reverse.Int(12345))
+}
